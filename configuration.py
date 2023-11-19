cba71e8a0e4eb9a04705c55c5a8e567dce574ed7
ArtemNureev <94983307+ArtemNureev@users.noreply.github.com>
2023-11-19 16:45:20 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Нуреев Артем, 10-я когорта — Финальный проект. Инженер по тестированию плюс
+
+URL_SERVICE = "https://7b6b5ade-dbda-4523-aad6-50978727d2a9.serverhub.praktikum-services.ru"
+CREAT_ORDERS = "/api/v1/orders"
